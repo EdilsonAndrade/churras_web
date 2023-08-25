486fe31ee8a1271fc4dc4f467dad725ee7f4fcb2
edilsonandrade <edilson.a.andrade@gmail.com>
2023-08-25 15:44:41 -0300

    refactor: clean up unused code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux'
 import store from '../redux/store'
 import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
-import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
