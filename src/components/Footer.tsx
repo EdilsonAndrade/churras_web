34ac8127088e74bf418ab7f9fe26ca17d1c49c2a
edilsonandrade <edilson.a.andrade@gmail.com>
2023-08-25 15:34:07 -0300

    feat: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import classNames from "classnames";
+import Image from "next/image"
+import { useRouter } from "next/router"
+export const Footer = () => {
+  const router = useRouter()
+  const routerPath = router.pathname;
+
+  return (
+
+    <div className={classNames('flex justify-center w-full pb-5', {
+      'bg-white p-1': routerPath === '/schedule',
+    })}>
+      <Image src={"/assets/trinca.png"} width={48} height={48} alt="Trinca" />
+    </div>
+  )
+}
