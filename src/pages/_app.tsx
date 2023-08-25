9acbfca16d2bfdfb38f6669ad5c4b84b8308a27d
edilsonandrade <edilson.a.andrade@gmail.com>
2023-08-25 15:36:24 -0300

    feat: adding header and footer to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
+import store from '../redux/store'
+import { Header } from '@/components/Header'
+import { Footer } from '@/components/Footer'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Header />
+
+      <Component {...pageProps} />
+
+      <Footer />
+    </Provider>
+  )
 }
