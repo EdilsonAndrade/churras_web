d18c16b52219ccdc41d5093223173625d15859ab
edilsonandrade <edilson.a.andrade@gmail.com>
2023-08-26 13:31:33 -0300

    refactor: convert date to a correct date format

--- src/components/Barbecue/BarbecueHeader.tsx
+++ src/components/Barbecue/BarbecueHeader.tsx
@@ -1,9 +1,13 @@
 import { Barbecue } from "@/common/types"
 
-export const BarbecueHeader = ({ description, date, observation }: Barbecue) => (
-  <div className="flex flex-col">
-    <strong>{date.toLocaleDateString('pt-BR', { month: '2-digit', day: '2-digit' })}</strong>
-    <strong>{description}</strong>
-    <small className="text-gray-400">{observation}</small>
-  </div>
-)
+export const BarbecueHeader = ({ description, date, observation }: Barbecue) => {
+  const eventDate = new Date(date.toString())
+  return (
+    <div className="flex flex-col">
+      <strong>{eventDate.toLocaleDateString('pt-BR', { month: '2-digit', day: '2-digit' })}</strong>
+      <strong>{description}</strong>
+      <small className="text-gray-400">{observation}</small>
+    </div>
+  )
+
+}
