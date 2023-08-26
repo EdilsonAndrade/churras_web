5cf92805ada403f3717c5f32e228df67c92d6d83
edilsonandrade <edilson.a.andrade@gmail.com>
2023-08-26 13:39:12 -0300

    feat: add post method

--- src/components/Barbecue/NewBarbecueEvent.tsx
+++ src/components/Barbecue/NewBarbecueEvent.tsx
@@ -2,12 +2,16 @@ import React, { useState } from 'react'
 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import classNames from 'classnames';
 import { useDispatch } from 'react-redux';
-import { addBarbecue } from '@/redux/reducers/barbecueReducer';
+import { setBarbecues } from '@/redux/reducers/barbecueReducer';
+import { Response } from '@/common/types';
 
+
+const apiRoute = process.env.NEXT_PUBLIC_API_URL
 
 export const NewBarbecueEvent = ({ onClose }: { onClose: () => void }) => {
-  const [startDate, setStartDate] = useState(new Date());
+  const [eventDate, setEventDate] = useState(new Date());
   const [description, setDescription] = useState('');
   const [suggestedValue, setSuggestedValue] = useState(0);
   const [observation, setObservation] = useState('');
@@ -16,21 +20,33 @@ export const NewBarbecueEvent = ({ onClose }: { onClose: () => void }) => {
   const dispatch = useDispatch();
   const handleSaveEvent = async (e: React.MouseEvent) => {
     e.preventDefault();
-    dispatch(addBarbecue({
-      date: startDate,
-      description,
-      suggestedValue,
-      observation,
-      id: Math.floor(Math.random() * 1000).toString()
-    }))
 
+    const response = await fetch(apiRoute + '/barbecue', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        date: eventDate,
+        description,
+        suggestedValue,
+        observation,
+        id: Math.floor(Math.random() * 1000).toString()
+      })
+    })
+    const data = await response.json() as Response;
+
+    dispatch(setBarbecues(data.barbecues))
     onClose();
   }
+
+  const disableSave = !description || !eventDate
+
 
   return (
     <div className="flex flex-col  justify-center p-4 rounded-sm shadow-md bg-white" >
       <label htmlFor="description">Data do churras</label>
-      <DatePicker className='border border-amber-300 mb-9' minDate={today} selected={startDate} onChange={(date: Date) => setStartDate(date)} dateFormat="dd/MM/yyyy" />
+      <DatePicker className='border border-amber-300 mb-9' minDate={today} selected={eventDate} onChange={(date: Date) => setEventDate(date)} dateFormat="dd/MM/yyyy" />
 
       <label htmlFor="description">Descrição</label>
       <input onChange={(e) => setDescription(e.target.value)} value={description} type="text" placeholder='Descrição' className="mb-9 border border-amber-300" />
@@ -41,7 +57,9 @@ export const NewBarbecueEvent = ({ onClose }: { onClose: () => void }) => {
 
       <div className='flex justify-between mt-10'>
         <button type="button" className='bg-red-500 text-white rounded-md w-36' onClick={() => onClose()}>Cancelar</button>
-        <button type='button' className='ml-2 p-3 bg-black text-white rounded-md w-36' onClick={handleSaveEvent}>Salvar</button>
+        <button type='button' disabled={disableSave} className={classNames('ml-2 p-3 bg-black text-white rounded-md w-36', {
+          'opacity-50 cursor-not-allowed': disableSave
+        })} onClick={handleSaveEvent}>Salvar</button>
       </div>
     </div>
   )
