6de17be3cbf831caa997ca2fe45ba10f482ce8e8
edilsonandrade <edilson.a.andrade@gmail.com>
2023-08-26 13:40:15 -0300

    feat: add delete participant and put to update status of payment

--- src/components/Participants/ParticipantList.tsx
+++ src/components/Participants/ParticipantList.tsx
@@ -1,28 +1,50 @@
 import { useDispatch } from 'react-redux'
-import { changePaidStatus, removeParticipant } from '@/redux/reducers/participantReducer'
+import { changePaidStatus, setParticipant } from '@/redux/reducers/participantReducer'
 import classNames from 'classnames';
 import { Trash } from 'react-feather'
 import Image from 'next/image';
+import React from 'react';
+import { Response } from '@/common/types';
 
 export const ParticipantList = ({ id, name, paid, amount, drink = 0 }: { id: string, name: string, paid: boolean, amount: number, drink: number }) => {
   const dispatch = useDispatch();
-
-  const handlePaid = () => {
+  const handlePaid = async () => {
+    await fetch(process.env.NEXT_PUBLIC_API_URL + '/participant', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        id,
+        paid: !paid,
+        name,
+        amount
+      })
+    })
     dispatch(changePaidStatus(id))
   }
 
-  const handleDelete = () => {
-    dispatch(removeParticipant(id))
+  const handleDelete = async () => {
+    const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/participant', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        id,
+
+      })
+    })
+
+    const data = await response.json() as Response;
+    dispatch(setParticipant(data.participants))
   }
 
 
   return (
     <div className="flex px-5 items-center justify-between font-semibold border border-t-0 border-x-0 border-amber-300">
       <div className="flex items-center w-2/4 py-2">
-
-        <input type="radio" checked={paid} onClick={handlePaid} />
-
-
+        <input type="checkbox" onChange={handlePaid} checked={paid} className='invisible' />
         <span className="ml-3">{name}</span>
 
       </div>
