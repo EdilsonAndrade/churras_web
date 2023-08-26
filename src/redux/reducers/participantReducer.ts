0df884819377594c80688b5b646cad59cb22fab7
edilsonandrade <edilson.a.andrade@gmail.com>
2023-08-26 13:42:09 -0300

    refactor: participant status with set and use the shared type

--- src/redux/reducers/participantReducer.ts
+++ src/redux/reducers/participantReducer.ts
@@ -1,13 +1,6 @@
+import { Participant } from "@/common/types";
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-interface Participant {
-  id: string;
-  name: string;
-  barbecueId: string;
-  amount: number;
-  drink: number
-  paid: boolean;
-}
 
 const initialState = {
   participants: [] as Participant[]
@@ -17,11 +10,8 @@ const participantSlice = createSlice({
   name: 'participant',
   initialState,
   reducers: {
-    addParticipant(state, action: PayloadAction<Participant>) {
-      state.participants.push(action.payload);
-    },
-    removeParticipant(state, action: PayloadAction<string>) {
-      state.participants = state.participants.filter(p => p.id !== action.payload);
+    setParticipant(state, action: PayloadAction<Participant[]>) {
+      state.participants =action.payload;
     },
     changePaidStatus(state, action: PayloadAction<string>) {
       const participant = state.participants.find(p => p.id === action.payload);
@@ -33,6 +23,6 @@ const participantSlice = createSlice({
   }
 });
 
-export const { addParticipant, changePaidStatus, removeParticipant } = participantSlice.actions;
+export const { setParticipant, changePaidStatus } = participantSlice.actions;
 
 export default participantSlice.reducer;
