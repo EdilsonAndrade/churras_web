4db5c5d5631a6b2158906bf49ce7c208384b34e2
edilsonandrade <edilson.a.andrade@gmail.com>
2023-08-27 18:43:19 -0300

    feat: signup page

---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,12 @@
+import { UserForm } from "@/components/User/UserForm";
+
+export default function SignUp() {
+  return (
+    <main
+      className="flex min-h-screen flex-col items-center pt-10"
+    >
+      <UserForm />
+    </main>
+
+  )
+}
