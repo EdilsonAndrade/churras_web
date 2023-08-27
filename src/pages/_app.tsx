43d14a84d464533defaf84c087e078d13922e857
edilsonandrade <edilson.a.andrade@gmail.com>
2023-08-27 18:39:46 -0300

    feat: adding toastify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,14 @@ import { Provider } from 'react-redux'
 import store from '../redux/store'
 import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <ToastContainer />
       <Header />
 
       <Component {...pageProps} />
