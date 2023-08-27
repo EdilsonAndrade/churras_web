df29d6796476281b2d9bd6225a45fd0a1c673b0b
edilsonandrade <edilson.a.andrade@gmail.com>
2023-08-27 18:43:09 -0300

    refactor:  adjust to have user and some code clean up

--- src/pages/api/barbecue.ts
+++ src/pages/api/barbecue.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import fs from 'fs'
 import path from 'path'
 import { Barbecue } from '@/common/types'
-import {Response} from '@/common/types'
+import { Response } from '@/common/types'
 
 const filePath = path.join(process.cwd(), 'data', 'churras.json')
 
@@ -11,17 +11,17 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Response>
 ) {
-  if(req.method === 'POST') {
+  if (req.method === 'POST') {
     return handlePost(req, res)
   }
-  if(req.method === 'GET'){
-    const response =  handleGet(req, res)
+  if (req.method === 'GET') {
+    const response = handleGet(req, res)
     return res.status(200).json(response)
   }
-  if(req.method === 'DELETE'){
+  if (req.method === 'DELETE') {
     return handleDelete(req, res)
   }
-  
+
 }
 
 const handleGet = (req: NextApiRequest, res: NextApiResponse<Response>) => {
@@ -32,14 +32,10 @@ const handleGet = (req: NextApiRequest, res: NextApiResponse<Response>) => {
 const handlePost = (req: NextApiRequest, res: NextApiResponse<Response>) => {
   const { id, description, date, observation, suggestedValue } = req.body
   const data = handleGet(req, res)
-  if(!data["barbecues"])
-  {
+  if (!data["barbecues"]) {
     data.barbecues = []
   }
-  if(!data["participants"])
-  {
-    data.participants = []
-  }
+
   data.barbecues.push({ id, description, date, observation, suggestedValue })
 
   fs.writeFileSync(filePath, JSON.stringify(data))
@@ -48,15 +44,15 @@ const handlePost = (req: NextApiRequest, res: NextApiResponse<Response>) => {
 
 const handleDelete = (req: NextApiRequest, res: NextApiResponse<Response>) => {
   const { id } = req.body
-  
+
   const data = handleGet(req, res)
   const barbecueIndex = data.barbecues.findIndex((barbecue: Barbecue) => barbecue.id === id)
-  if(barbecueIndex >=0 ) {
+  if (barbecueIndex >= 0) {
     data.barbecues.splice(barbecueIndex, 1)
     data.participants.splice(data.participants.findIndex((participant: any) => participant.barbecueId === id))
     fs.writeFileSync(filePath, JSON.stringify(data))
   }
-  
+
   return res.status(200).json(data)
 }
 
--- src/pages/api/participant.ts
+++ src/pages/api/participant.ts
@@ -4,45 +4,46 @@ import fs from 'fs'
 import path from 'path'
 import { Participant } from '@/common/types'
 
-import {Response} from '@/common/types'
+import { Response } from '@/common/types'
 const filePath = path.join(process.cwd(), 'data', 'churras.json')
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Response>
 ) {
-  if(req.method === 'POST') {
+  if (req.method === 'POST') {
     return handlePost(req, res)
   }
-  if(req.method === 'GET'){
-    const response =  handleGet(req, res)
+  if (req.method === 'GET') {
+    const response = handleGet(req, res)
     return res.status(200).json(response)
   }
-  if(req.method === 'DELETE'){
+  if (req.method === 'DELETE') {
     return handleDelete(req, res)
   }
-  if(req.method === 'PUT'){
+  if (req.method === 'PUT') {
     return handlePut(req, res)
   }
 
-  
+
 }
+
 
 const handleGet = (req: NextApiRequest, res: NextApiResponse<Response>) => {
   const fileData = fs.readFileSync(filePath)
   const data = JSON.parse(fileData.toString()) as Response
   const participants = data.participants || [];
   const barbecues = data.barbecues || [];
-  
-  return {participants, barbecues}
-  
+  const users = data.users || [];
+
+  return { participants, barbecues, users }
+
 }
 
 const handlePost = (req: NextApiRequest, res: NextApiResponse<Response>) => {
   const { id, barbecueId, name, amount, drink, paid } = req.body
   const data = handleGet(req, res)
-  if(!data["participants"])
-  {
+  if (!data["participants"]) {
     data.participants = []
   }
   data.participants.push({ id, barbecueId, name, amount, drink, paid })
@@ -54,20 +55,20 @@ const handleDelete = (req: NextApiRequest, res: NextApiResponse<Response>) => {
   const { id } = req.body
   const data = handleGet(req, res)
   const participantIndex = data.participants.findIndex((participant: Participant) => participant.id === id)
-  if(participantIndex >=0){
+  if (participantIndex >= 0) {
     data.participants.splice(participantIndex, 1)
     fs.writeFileSync(filePath, JSON.stringify(data))
   }
-  
+
   return res.status(200).json(data)
 }
 
 const handlePut = (req: NextApiRequest, res: NextApiResponse<Response>) => {
   const { id } = req.body
-  
+
   const data = handleGet(req, res)
   const participantIndex = data.participants.findIndex((participant: Participant) => participant.id === id)
-  if(participantIndex >=0) {
+  if (participantIndex >= 0) {
     data.participants[participantIndex].paid = !data.participants[participantIndex].paid
     fs.writeFileSync(filePath, JSON.stringify(data))
   }
